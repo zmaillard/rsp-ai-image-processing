053c602cc90e994849e761744736d661e50a4d7c
Zachary Maillard <zmaillard@protonmail.com>
2026-02-23 07:21:41 -0700

    New Region

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 resource "digitalocean_droplet" "rsp_image_processing" {
   image    = "gpu-h100x1-base"
   name     = "rsp-image-processing"
-  region   = "tor1"
-  size     = "gpu-6000adax1-48gb"
+  region   = "nyc2"
+  size     = "gpu-h100x1-80gb"
   ssh_keys = [data.digitalocean_ssh_key.terraform.id]
 }
 
