1c353b7d37f54b699005118a0bcf2f1910b24217
Zachary Maillard <zmaillard@protonmail.com>
2026-02-22 20:00:06 -0700

    again

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,12 @@
 output "droplet_ip" {
   value = digitalocean_droplet.rsp_image_processing.ipv4_address
 }
+
+resource "local_file" "inventory_file" {
+  content = templatefile("./inventory.template",
+    {
+      digitalocean_public_ip = [digitalocean_droplet.rsp_image_processing.ipv4_address]
+    }
+  )
+  filename = "./inventory"
+}
