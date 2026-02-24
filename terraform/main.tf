5accd475868190ff95786950aeae840680abf347
Zachary Maillard <zmaillard@protonmail.com>
2026-02-23 20:18:54 -0700

    size

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "digitalocean_droplet" "rsp_image_processing" {
   image    = "gpu-h100x1-base"
   name     = "rsp-image-processing"
   region   = "tor1"
-  size     = "gpu-l40sx1-48gb"
+  size     = "gpu-h100x1-80gb"
   ssh_keys = [data.digitalocean_ssh_key.terraform.id]
 }
 
