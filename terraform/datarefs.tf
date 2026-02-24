213e9619cf4c239e0a60866c57a9217995412155
Zach Maillard <zmaillard@protonmail.com>
2026-02-24 07:27:23 -0700

    Removing Packer Images (#1)
    * Consolidate Ansible Workflow into Terraform
    
    * Ansible inventory
    
    * fix script
    
    * again
    
    * Moving
    
    * New Region
    
    * Fixing
    
    * correct path
    
    * Path
    
    * Tor
    
    * Again
    
    * again
    
    * size
    
    * Removing Packer
    
    * Adding SSH

--- terraform/datarefs.tf
+++ terraform/datarefs.tf
@@ -2,8 +2,3 @@ data "digitalocean_ssh_key" "terraform" {
   name = "Automation"
 }
 
-data "digitalocean_droplet_snapshot" "packer_snapshot" {
-  name_regex  = "packer-1770866861"
-  region      = "tor1"
-  most_recent = true
-}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,21 @@
 resource "digitalocean_droplet" "rsp_image_processing" {
-  image    = data.digitalocean_droplet_snapshot.packer_snapshot.id
+  image    = "gpu-h100x1-base"
   name     = "rsp-image-processing"
   region   = "tor1"
-  size     = "gpu-6000adax1-48gb"
+  size     = "gpu-h100x1-80gb"
   ssh_keys = [data.digitalocean_ssh_key.terraform.id]
 }
 
+resource "null_resource" "rsp_image_processing" {
+  provisioner "remote-exec" {
+    connection {
+      host = digitalocean_droplet.rsp_image_processing.ipv4_address 
+      user = "root"
+      private_key = file("${path.module}/ssh_key")
+    }
+
+    inline = ["echo 'connected!'"]
+  }
+
+}
 
