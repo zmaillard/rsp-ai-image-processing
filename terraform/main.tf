43ddd9e17b01ea9a5130ef0ae038f2e132ff1fb9
Zachary Maillard <zmaillard@protonmail.com>
2026-02-23 20:14:56 -0700

    again

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,16 @@ resource "digitalocean_droplet" "rsp_image_processing" {
   ssh_keys = [data.digitalocean_ssh_key.terraform.id]
 }
 
+resource "null_resource" "rsp_image_processing" {
+  provisioner "remote-exec" {
+    connection {
+      host = digitalocean_droplet.rsp_image_processing.ipv4_address 
+      user = "root"
+      private_key = file("${path.module}/ssh_key")
+    }
+
+    inline = ["echo 'connected!'"]
+  }
+
+}
+
